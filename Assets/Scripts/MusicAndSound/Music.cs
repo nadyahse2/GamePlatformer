f69ea83dbfe12666fad72ea4d0137fa2942d82ee
immedeg <145657237+immedeg@users.noreply.github.com>
2024-11-14 06:20:18 +0300

    Sounds
    Level 5: edit some details

--- Assets/Scripts/MusicAndSound/Music.cs
+++ Assets/Scripts/MusicAndSound/Music.cs
@@ -3,17 +3,21 @@ using UnityEngine;
 
 public class Music : MonoBehaviour
 {
+    [SerializeField] AudioSource soundSource;
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
 
     public AudioClip background;
+    public AudioClip music;
     public AudioClip death;
     public AudioClip checkpoint;
     public AudioClip walltouch;
 
     private void Start()
     {
-        musicSource.clip = background;
+        soundSource.clip = background;
+        musicSource.clip = music;
         musicSource.Play();
+        soundSource.Play();
     }
 }
